57d5935c869090e2c901deac4350ec8fe54cadc9
v0idpwn <v0idpwn@gmail.com>
2021-11-12 18:44:51 +0200

    Add dependencies

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Plock.MixProject do
     [
       app: :plock,
       version: "0.1.0",
-      elixir: "~> 1.13-dev",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,8 +21,8 @@ defmodule Plock.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ecto_sql, "~> 3.7"},
+      {:postgrex, "~> 0.15"},
     ]
   end
 end
